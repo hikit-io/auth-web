dd3998568057f4b89d9f8f6266eddc0f3a3c5e4e
Nekilc <nieaowei@gmail.com>
2023-02-14 18:58:21 +0800

    deploy

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,11 +1,15 @@
 <script lang="ts" setup>
+import {useService} from "../compose/useService";
+
+const client = useService()
+
 
 </script>
 
 <template>
-<a-card>
-  <a-typography-title>Hi, {{}}</a-typography-title>
-</a-card>
+  <a-card>
+    <a-typography-title>Hi, {{}}</a-typography-title>
+  </a-card>
 </template>›
 
 <style scoped>
